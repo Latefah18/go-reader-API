e2eb458884a85ce4537bd41e4d0ab5d7216393c9
Latefah18 <106754794+Latefah18@users.noreply.github.com>
2022-07-05 14:45:50 +0300

    Delete src directory

--- src/controller/find.ts
+++
@@ -1,8 +0,0 @@
-
-export function findbookcontroller(books: any[], x: any) {
-const book= books.filter((el) => el.books == x);
-
-	return (books);
-}
-
-
--- src/routes/login.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export default async function (server: FastifyInstance) {
-	server.get('/login', async (request, reply) => {
-		return 'wecome to reader word ';
-	});
-
-	
-}
